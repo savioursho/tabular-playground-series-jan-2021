e0f394681c1f5241cd49257d74b78b2dffb0be9e
saviour <savioursho@gmail.com>
2023-01-18 07:55:31 +0900

    fix: joblibをインポートできるようにした

--- src/kagglebook/util.py
+++ src/kagglebook/util.py
@@ -2,13 +2,13 @@ import datetime
 import logging
 import os
 
+# from sklearn.externals import joblib
+import joblib
 import numpy as np
 import pandas as pd
-from sklearn.externals import joblib
 
 
 class Util:
-
     @classmethod
     def dump(cls, value, path):
         os.makedirs(os.path.dirname(path), exist_ok=True)
@@ -20,13 +20,12 @@ class Util:
 
 
 class Logger:
-
     def __init__(self):
-        self.general_logger = logging.getLogger('general')
-        self.result_logger = logging.getLogger('result')
+        self.general_logger = logging.getLogger("general")
+        self.result_logger = logging.getLogger("result")
         stream_handler = logging.StreamHandler()
-        file_general_handler = logging.FileHandler('../model/general.log')
-        file_result_handler = logging.FileHandler('../model/result.log')
+        file_general_handler = logging.FileHandler("../model/general.log")
+        file_result_handler = logging.FileHandler("../model/result.log")
         if len(self.general_logger.handlers) == 0:
             self.general_logger.addHandler(stream_handler)
             self.general_logger.addHandler(file_general_handler)
@@ -37,7 +36,7 @@ class Logger:
 
     def info(self, message):
         # 時刻をつけてコンソールとログに出力
-        self.general_logger.info('[{}] - {}'.format(self.now_string(), message))
+        self.general_logger.info("[{}] - {}".format(self.now_string(), message))
 
     def result(self, message):
         self.result_logger.info(message)
@@ -48,25 +47,24 @@ class Logger:
     def result_scores(self, run_name, scores):
         # 計算結果をコンソールと計算結果用ログに出力
         dic = dict()
-        dic['name'] = run_name
-        dic['score'] = np.mean(scores)
+        dic["name"] = run_name
+        dic["score"] = np.mean(scores)
         for i, score in enumerate(scores):
-            dic[f'score{i}'] = score
+            dic[f"score{i}"] = score
         self.result(self.to_ltsv(dic))
 
     def now_string(self):
-        return str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        return str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
     def to_ltsv(self, dic):
-        return '\t'.join(['{}:{}'.format(key, value) for key, value in dic.items()])
+        return "\t".join(["{}:{}".format(key, value) for key, value in dic.items()])
 
 
 class Submission:
-
     @classmethod
     def create_submission(cls, run_name):
-        submission = pd.read_csv('../input/sampleSubmission.csv')
-        pred = Util.load(f'../model/pred/{run_name}-test.pkl')
+        submission = pd.read_csv("../input/sampleSubmission.csv")
+        pred = Util.load(f"../model/pred/{run_name}-test.pkl")
         for i in range(pred.shape[1]):
-            submission[f'Class_{i + 1}'] = pred[:, i]
-        submission.to_csv(f'../submission/{run_name}.csv', index=False)
+            submission[f"Class_{i + 1}"] = pred[:, i]
+        submission.to_csv(f"../submission/{run_name}.csv", index=False)
